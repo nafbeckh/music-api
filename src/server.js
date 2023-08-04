21481e4a62a9a940dd2b9b369d95687cd37366dd
nafbeckh <nafbeckhshn@gmail.com>
2023-08-04 18:05:34 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const Hapi = require('@hapi/hapi')
+const albums = require('./api/albums')
+const AlbumsService = require('./services/inMemory/AlbumsService')
+const ClientError = require('./exceptions/ClientError')
 
 const init = async () => {
+  const albumsService = new AlbumsService()
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -12,8 +16,43 @@ const init = async () => {
     }
   })
 
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService
+    }
+  })
+
+  server.ext('onPreResponse', (req, h) => {
+    const { response } = req
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message
+        })
+
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+    }
+
+    if (!response.isServer) {
+      return h.continue
+    }
+
+    const newResponse = h.response({
+      status: 'error',
+      message: 'terjadi kegagalan pada server kami'
+    })
+
+    newResponse.code(500)
+    return newResponse
+  })
+
   await server.start()
-  console.log(`Server running on ${server.info.uri}`)
+  console.log(`Server berjalan di ${server.info.uri}`)
 }
 
 init()
