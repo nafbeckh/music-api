f93c6773a70f94844307a747033473b828dd61f4
nafbeckh <nafbeckhshn@gmail.com>
2023-08-05 21:12:55 +0700

    Edit Post & Put album models

--- src/utils/models/albums/PostAlbumModel.js
+++ src/utils/models/albums/PostAlbumModel.js
@@ -5,10 +5,12 @@ class PostAlbumModel {
     this.id = `album-${nanoid(16)}`
     this.name = name
     this.year = year
+    this.createdAt = new Date().toISOString()
+    this.updatedAt = this.createdAt
   }
 
   getInsertModel () {
-    return [this.id, this.name, this.year]
+    return [this.id, this.name, this.year, this.createdAt, this.updatedAt]
   }
 }
 
--- src/utils/models/albums/PutAlbumModel.js
+++ src/utils/models/albums/PutAlbumModel.js
@@ -2,11 +2,12 @@ class PutAlbumModel {
   constructor (id, { name, year }) {
     this.name = name
     this.year = year
+    this.updatedAt = new Date().toISOString()
     this.id = id
   }
 
   getUpdateModel () {
-    return [this.name, this.year, this.id]
+    return [this.name, this.year, this.updatedAt, this.id]
   }
 }
 
