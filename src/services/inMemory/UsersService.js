39cac49072d1b016b5238d9c2c785bb558b45384
nafbeckh <nafbeckhshn@gmail.com>
2023-08-08 10:40:25 +0700

    Create verifyUserCredential method

--- src/services/inMemory/UsersService.js
+++ src/services/inMemory/UsersService.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg')
 const bcrypt = require('bcrypt')
 const InvariantError = require('../../exceptions/InvariantError')
+const AuthenticationError = require('../../exceptions/AuthenticationError')
 const { PostUserModel } = require('../../utils/models/users')
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS, 10)
@@ -40,6 +41,29 @@ class UsersService {
     if (result.rows.length > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan')
     }
+  }
+
+  async verifyUserCredential ({ username, password }) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah')
+    }
+
+    const { id, password: hashedPassword } = result.rows[0]
+
+    const match = await bcrypt.compare(password, hashedPassword)
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah2')
+    }
+
+    return id
   }
 }
 
