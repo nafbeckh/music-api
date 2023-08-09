700614d92bde32019f7159277c48089cb625ed5d
nafbeckh <nafbeckhshn@gmail.com>
2023-08-09 17:17:35 +0700

    Edit playlists model

--- src/utils/models/playlists/index.js
+++ src/utils/models/playlists/index.js
@@ -1,5 +1,3 @@
 const PostPlaylistModel = require('./PostPlaylistModel')
-const GetAllPlaylistsModel = require('./GetAllPlaylistsModel')
-const GetPlaylistModel = require('./GetPlaylistModel')
 
-module.exports = { PostPlaylistModel, GetPlaylistModel, GetAllPlaylistsModel }
+module.exports = { PostPlaylistModel }
