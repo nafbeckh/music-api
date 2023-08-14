b2637afd0b0921c11a3060485c3dcf17a0c8fcd8
nafbeckh <nafbeckhshn@gmail.com>
2023-08-14 21:17:53 +0700

    Update server & register expors plugin

--- src/server.js
+++ src/server.js
@@ -37,6 +37,11 @@ const collaborations = require('./api/collaborations')
 const CollaborationsService = require('./services/postgres/CollaborationsService')
 const CollaborationsValidator = require('./validator/collaborations')
 
+/* Exports */
+const _exports = require('./api/exports')
+const producerService = require('./services/messageBroker/ProduserServices')
+const ExportsValidator = require('./validator/exports')
+
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT,
@@ -130,6 +135,14 @@ const init = async () => {
         collaborationsService,
         playlistsService,
         validator: CollaborationsValidator
+      }
+    },
+    {
+      plugin: _exports,
+      options: {
+        producerService,
+        playlistsService,
+        validator: ExportsValidator
       }
     }
   ])
